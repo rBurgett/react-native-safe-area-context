3778d82a3b7df42fbdfeb5914ea80219da1ea166
rBurgett <ryan@burgettdev.net>
2023-09-08 17:30:31 -0400

    Attempted to fix flashing on re-render

--- src/SafeAreaView.tsx
+++ src/SafeAreaView.tsx
@@ -1,23 +1,54 @@
 import * as React from 'react';
-import type { NativeSafeAreaViewProps } from './SafeArea.types';
-import NativeSafeAreaView from './specs/NativeSafeAreaView';
+import type { Edge } from './SafeArea.types';
+import {
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewProps,
+  ViewStyle,
+} from 'react-native';
+import type { ReactNode } from 'react';
+import { useSafeAreaInsets } from './SafeAreaContext';
 
-type NativeSafeAreaViewInstance = InstanceType<typeof NativeSafeAreaView>;
+type Props = {
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+  edges?: readonly Edge[] | undefined;
+} & ViewProps;
 
-export type SafeAreaViewProps = NativeSafeAreaViewProps;
-
-export const SafeAreaView: React.ForwardRefExoticComponent<
-  SafeAreaViewProps & React.RefAttributes<NativeSafeAreaViewInstance>
-> = React.forwardRef<NativeSafeAreaViewInstance, SafeAreaViewProps>(
-  ({ edges, ...props }, ref) => {
-    return (
-      <NativeSafeAreaView
-        {...props}
-        // Codegen doesn't support default values for array types so
-        // set it here.
-        edges={edges ?? ['bottom', 'left', 'right', 'top']}
-        ref={ref}
-      />
-    );
-  },
-);
+/**
+ * USE THIS - Alternative to the default [SafeAreaView](https://github.com/th3rdwave/react-native-safe-area-context#safeareaview)
+ * from react-native-safe-area-context which currently has an issue that will cause a flicker / jump on first render on iOS / Android.
+ *
+ * [SafeAreaProvider](https://github.com/th3rdwave/react-native-safe-area-context#safeareaprovider) should still be higher in the tree.
+ *
+ * GitHub issues:
+ * [219](https://github.com/th3rdwave/react-native-safe-area-context/issues/219),
+ * [226](https://github.com/th3rdwave/react-native-safe-area-context/issues/226)
+ */
+export function SafeAreaView({ children, style, edges, ...rest }: Props) {
+  const insets = useSafeAreaInsets();
+  const defaultEdges = edges === undefined;
+  return (
+    <View
+      style={StyleSheet.compose(
+        {
+          paddingTop:
+            defaultEdges || edges?.includes('top') ? insets.top : undefined,
+          paddingBottom:
+            defaultEdges || edges?.includes('bottom')
+              ? insets.bottom
+              : undefined,
+          paddingLeft:
+            defaultEdges || edges?.includes('left') ? insets.left : undefined,
+          paddingRight:
+            defaultEdges || edges?.includes('right') ? insets.right : undefined,
+        },
+        style,
+      )}
+      {...rest}
+    >
+      {children}
+    </View>
+  );
+}
